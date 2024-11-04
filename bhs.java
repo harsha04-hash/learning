68fd438a28379c9aa8afe0530e41764b5179ecc8
HarshavardhanreddyCSM <harshavardhanreddygudepu99@gmail.com>
2024-11-04 19:30:45 +0530

    just now learning

--- bhs.java
+++ bhs.java
@@ -2,6 +2,6 @@ import java.util.*;
 public class bhs{
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
-    System.out.println("Hello world! testing in java for learning in GIT");
+    System.out.println("Hello world testing in java for learning in GIT which is useful");
   }
 }
